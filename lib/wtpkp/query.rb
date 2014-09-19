2e174b0e35d3c821d82aacc8d6659bba596239ae
Piotrek Dubiel <piotr.dubiel@polidea.com>
2014-09-19 09:59:41 +0200

    fix bug, for realz

--- lib/wtpkp/query.rb
+++ lib/wtpkp/query.rb
@@ -19,12 +19,25 @@ module WTPKP
           form.field_with(id: "time").value = @time.strftime("%H:%M")
         end
         departures_page = form.submit(form.button_with(name: 'start'))
-        departures = departures_page.search('.tpOverview > :nth-child(5), .selected > :nth-child(5)').map do |departure|
-          departure.children.map(&:to_s).select { |t| t =~ /\d\d:\d\d/ }
-        end.map do |record|
-          Course.new origin: @origin, destination: @destination, departure: record[0], arrival: record[1]
+        departures = departures_page.search('.tpOverview > :nth-child(5), .selected > :nth-child(5)')
+          .map do |departure|
+            departure.children.map(&:to_s).select { |t| t =~ /\d\d:\d\d/ }
+          end
+        stations = departures_page.search('.tpOverview > :nth-child(2), .selected > :nth-child(2)')
+          .map do |station|
+            station.children
+              .select { |c| c.is_a? Nokogiri::XML::Text }
+              .map(&:to_s)
+              .map(&:strip)
+              .select { |c| not c.empty? }
+              .compact
+          end
+        stations.select! { |s| not s.empty? }
+
+        result = stations.zip(departures).map do |record|
+          Course.new origin: record[0][0], destination: record[0][1], departure: record[1][0], arrival: record[1][1]
         end
-        return departures.sort
+        return result.sort
       end
     end
   end
